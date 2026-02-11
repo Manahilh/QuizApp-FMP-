9f0eb7b293590bde2939809d715b549039bf16b1
Manahilh <140234289+Manahilh@users.noreply.github.com>
2026-02-11 21:21:23 +0500

    Refactor Firebase configuration and initialization

--- firebase.config.js
+++ firebase.config.js
@@ -1,4 +1,3 @@
-// Firebase configuration object containing keys and identifiers for the app
 var firebaseConfig = {
     apiKey: "AIzaSyAwBKZbj7SrsQbLLH9T3Dr3eA_mXu54E3k",
     authDomain: "quizapp-4ce34.firebaseapp.com",
@@ -8,12 +7,9 @@ var firebaseConfig = {
     appId: "1:1039516347141:web:22637abe16ba8f4c11a56c"
 };
 
-// Initialize Firebase only if it hasn't been initialized yet
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
-// Export the authentication instance for use in other files
-
 const auth = firebase.auth();
-const db = firebase.firestore();
+const db = firebase.firestore(); 
